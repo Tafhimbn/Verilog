3b29d351bff4292f03575a305b79f062206550a4
Tafhim Bin Nasir <47665581+Tafhimbn@users.noreply.github.com>
2022-11-10 11:22:19 +0600

    Create counter_test.v

---
+++ counter_test.v
@@ -0,0 +1,23 @@
+module test_counter;
+	reg clk, clr;
+	wire [7:0] out;
+	
+	counter cnt (clr, clk, out);
+	
+	initial clk=1'b0;
+	always #5 clk=~clk;
+	initial
+		begin
+			clr = 1'b1;
+			#15 clr = 1'b0;
+			#200 clr = 1'b0;
+			#10 $finish;
+		end
+	
+		initial
+			begin
+				$dumpfile ("counter.vcd");
+				$dumpvars (0, test_counter);
+				$monitor ($time, "cout: %d", out);
+			end
+endmodule
