56ba25d7b695b2527835a7cc0983adfc8a2d4bf8
Tafhim Bin Nasir <47665581+Tafhimbn@users.noreply.github.com>
2022-11-14 21:17:16 +0600

    Update full_adder_test.v

--- full_adder_test.v
+++ full_adder_test.v
@@ -1,14 +1,19 @@
-module full_adder_test;
+`timescale 1ns/1ps
+module testbench;
 	reg a,b,c;
 	wire sum, cout;
 	full_adder FA(sum,cout,a,b,c);
 	initial
 	begin 
 		$monitor ($time,"a=%b,b=%b,c=%b,sum=%b,cout=%b",a,b,c,sum,cout);
-		#5 a=0; b=0; c=1;
-		#5 b=1;
-		#5 a=1;
-		#5 a=0; b=0; c=0;
+		#5 c=0; b=0; a=0;
+		#5 b=0; a=1; 
+		#5 b=1; a=0;
+		#5 b=1;	a=1;
+		#5 c=1;	b=0; a=0;
+		#5 b=0; a=1; 
+		#5 b=1; a=0;
+		#5 b=1;	a=1;
 		#5 $finish;
 	end			
-endmodul
+endmodule
