bc58f255f1dfc5188d437676389dd97311185b70
Tafhim Bin Nasir <47665581+Tafhimbn@users.noreply.github.com>
2022-11-16 20:30:05 +0600

    Update Parity_detector.v

--- Parity_detector.v
+++ Parity_detector.v
@@ -6,7 +6,7 @@ module parity_gen_9bit(d,even,odd);
 	xor x1(d01,d[0],d[1]),  x2(d23,d[2],d[3]),
 	    x3(d45,d[4],d[5]),  x4(d67,d[6],d[7]),
 	    x5(d0_3,d01,d23),   x6(d4_7,d45,d67),
-		x7(d0_7,d0_3,d4_7), x8(even,d0_7,d[8]);	
+	    x7(d0_7,d0_3,d4_7), x8(even,d0_7,d[8]);	
 	
 	not e_inv(odd,even); 
 
