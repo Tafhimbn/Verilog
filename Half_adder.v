fda8ffec7aeeff7c404aac930c9afe81cb07b567
Tafhim Bin Nasir <47665581+Tafhimbn@users.noreply.github.com>
2022-11-20 20:14:48 +0600

    Update Half_adder.v

--- Half_adder.v
+++ Half_adder.v
@@ -1,3 +1,15 @@
+// Dataflow modeling
+
+module half_adder(a,b,sum,cout);
+	input a,b;
+	output sum,cout;
+	
+	assign sum = a^b;
+	assign cout = (~a&b)|(a&~b);  
+	
+endmodule
+
+/*
 // Behavioral modeling 
 
 module half_adder(a,b,sum,cout);
@@ -9,3 +21,19 @@ always@(a,b)
 	 cout = a & b;
 	end
 endmodule
+
+	
+
+//Structural / gatelevel modeling
+
+module half_adder(a,b,sum,cout);
+	input a, b;
+	output sum, cout;
+
+	xor x_ab(sum,a,b);
+	and and_ab(cout,a,b);
+
+endmodule		
+
+*/
+	
