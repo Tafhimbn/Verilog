1554ea08d531044bdd45566309449b941bb642a5
Tafhim Bin Nasir <47665581+Tafhimbn@users.noreply.github.com>
2022-12-11 15:50:55 +0600

    Update johnson_counter.v

--- johnson_counter.v
+++ johnson_counter.v
@@ -22,3 +22,40 @@ module johnson_counter(clk,rstn,out);
           end  
        end  
 endmodule  
+
+
+// Structural / gatelevel modeling	
+
+module d_ff(q,qbar,clk, d,rst);
+	input d, rst, clk;
+	output reg q;
+	output qbar;	 
+	
+	always@(posedge clk)
+		begin 
+			if(!rst) 
+			begin q = 0; end
+			else 
+			begin q = d;  end
+		 end 
+	
+	assign qbar = ~q;
+endmodule
+
+
+
+module ring_counter(clk, rst,out);
+	input clk, rst; 
+	parameter n = 4;
+	output [n-1:0] out;
+	
+	wire  q0, qn0, q1, qn1,q2,qn2, q3,qn3;
+	
+	d_ff df1 (q0, qn0, clk, q1, rst),
+	     df2 (q1, qn1, clk, q2, rst),
+	     df3 (q2, qn2, clk, q3, rst),
+	     df4 (q3, qn3, clk, ~q0, rst);	 
+	
+   assign out = {q0, q1, q2, q3}; 
+		 
+endmodule
